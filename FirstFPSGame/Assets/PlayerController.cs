032f699335030592eaaa405c711468cc47778f0b
berry <j890093@gmail.com>
2018-09-22 17:55:54 +0800

    another Marterials and Player Move Forward

---
+++ FirstFPSGame/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator AnimatorController;
+	// Use this for initialization
+	void Start () {
+        AnimatorController = this.GetComponent<Animator>();
+	}
+
+    public float m_fMoveSpeed;
+    float nCurrentSpeed = 0;
+
+	// Update is called once per frame
+	void Update () {
+        float fResult = 0;
+        if (Input.GetKey(KeyCode.W))
+            fResult += m_fMoveSpeed;
+
+        nCurrentSpeed = fResult;
+
+        this.transform.position += Time.deltaTime * nCurrentSpeed * this.transform.forward;
+
+        AnimatorController.SetFloat("Speed", nCurrentSpeed);
+    }
+}
